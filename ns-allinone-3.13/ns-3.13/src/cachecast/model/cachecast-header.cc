75c91e8fbdafc2b53181f5e249140c4a652755a5
daghso <daghso@student.uio.no>
2012-03-11 11:32:57 +0100

    General additions to CacheCast header

--- ns-allinone-3.13/ns-3.13/src/cachecast/model/cachecast-header.cc
+++ ns-allinone-3.13/ns-3.13/src/cachecast/model/cachecast-header.cc
@@ -1,11 +1,71 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
+#include "ns3/log.h"
 #include "cachecast-header.h"
+
+NS_LOG_COMPONENT_DEFINE ("CacheCastHeader");
 
 namespace ns3 {
 
-/* ... */
+NS_OBJECT_ENSURE_REGISTERED (CacheCastHeader);
 
-
+CacheCastHeader::CacheCastHeader ()
+  : m_payloadId (0),
+    m_payloadSize (0),
+    m_originalSize (0)
+{
 }
+
+uint32_t
+CacheCastHeader::GetPayloadId (void) const
+{
+  return m_payloadId;
+}
+
+TypeId
+CacheCastHeader::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::CacheCastHeader")
+    .SetParent<Header> ()
+    .AddConstructor<CacheCastHeader> ();
+
+  return tid;
+}
+
+TypeId
+CacheCastHeader::GetInstanceTypeId (void) const
+{
+  return GetTypeId ();
+}
+
+void
+CacheCastHeader::Print (std::ostream &os) const
+{
+  os << "payload ID " << m_payloadId << " "
+     << "payload size " << m_payloadSize << " "
+     << "original size " << m_originalSize
+  ;
+}
+
+uint32_t
+CacheCastHeader::GetSerializedSize (void) const
+{
+  // TODO figure out serialized size
+  return 0;
+}
+
+void
+CacheCastHeader::Serialize (Buffer::Iterator start) const
+{
+  // TODO
+}
+
+uint32_t
+CacheCastHeader::Deserialize (Buffer::Iterator start)
+{
+  //TODO
+  return 0;
+}
+
+} // namespace ns3
 
--- ns-allinone-3.13/ns-3.13/src/cachecast/model/cachecast-header.h
+++ ns-allinone-3.13/ns-3.13/src/cachecast/model/cachecast-header.h
@@ -2,11 +2,41 @@
 #ifndef __CACHECAST_HEADER_H__
 #define __CACHECAST_HEADER_H__
 
+#include "ns3/header.h"
+
 namespace ns3 {
 
-/* ... */
+/**
+ * \brief Header for CacheCast packet
+ */
+class CacheCastHeader : public Header
+{
+public:
+  /**
+   * \brief Construct an empty CacheCastHeader
+   */
+  CacheCastHeader ();
 
-}
+  /**
+   * \returns the payload ID of this packet
+   */
+  uint32_t GetPayloadId (void) const;
+
+  static TypeId GetTypeId (void);
+  TypeId GetInstanceTypeId (void) const;
+  void Print (std::ostream &os) const;
+  uint32_t GetSerializedSize (void) const;
+  void Serialize (Buffer::Iterator start) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+
+private:
+  uint32_t m_payloadId;
+  uint32_t m_payloadSize;
+  uint32_t m_originalSize;
+
+};
+
+} // namespace ns3
 
 #endif /* __CACHECAST-HEADER_H__ */
 
